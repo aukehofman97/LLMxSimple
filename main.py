92c48fd705da694ebd7997fff88effecf0648439
Auke Hofman <aukehofman97@gmail.com>
2024-09-17 16:09:41 +0200

    also main included

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ def main():
     st.set_page_config(layout='wide')
     st.title('LLM for Logistics Data Mapping')
 
-    tab1, tab2 = st.tabs(["Own Structure ➡️ FEDeRATED", "FEDeRATED ➡️ Own Structure"])
+    tab1, tab2, tab3 = st.tabs(["Own Structure ➡️ FEDeRATED", "FEDeRATED ➡️ Own Structure", "Event Type Detector and Transformer"])
 
     with tab1:
         app.tab_one()
 
     with tab2:
         app.tab_two()
+    with tab3:
+        app.tab_three
 
 if __name__ == "__main__":
     main()
