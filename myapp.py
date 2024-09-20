af0b03ccea1cf98a2b69b819158cb6b9e4ea8c42
Auke Hofman <aukehofman97@gmail.com>
2024-09-20 12:02:56 +0200

    maybe?

--- myapp.py
+++ myapp.py
@@ -5,9 +5,8 @@ from openai import OpenAI
 import pandas as pd
 import json
 import os
-from rdflib import Graph, RDF, RDFS, OWL
 
-from utils.utils import *
+from utils.utils import read_file, read_predefined_file, save_text, get_openai_response, save_user_info, extract_event_uuids, extract_main_classes, extract_object_fields, update_field_value, extract_all_fields, find_field_value, process_assistance_option, extract_event_type_from_response, process_event_type, ttl_parser, extract_local_name
 from models import *
 
 client = OpenAI(api_key=st.secrets["OPEN_API_KEY"])
